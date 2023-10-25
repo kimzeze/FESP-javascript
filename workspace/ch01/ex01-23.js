05cc4f008917509a17c767807b66c3ea8228faca
kimzeze <fridaynight@kakao.com>
2023-10-25 13:07:01 +0900

    2일차 파일 세팅 및 오전 수업 코드

--- workspace/ch01/ex01-23.js
+++ workspace/ch01/ex01-23.js
@@ -1,8 +1,9 @@
 // 함수의 매개변수에 적용
-function fn(a, b) {
-  console.log(a, b);
+// function fn(a, b, ...rest) {
+function fn(a, b, ...rest) {
+  console.log(a, b, rest);
 }
-    
+
 fn();
 fn(1);
 fn(2, 3);
@@ -10,3 +11,16 @@ fn(4, 5, 6);
 fn(7, 8, 9, 10, 11);
 
 // 구조 분해 할당에서 사용
+
+var [first, second, ...rest] = [100, 200, 300, 400, 500];
+console.log(first, second, rest);
+
+var user = {
+  name: "김도현",
+  age: 28,
+  phone: "1234",
+  address: "서울시",
+};
+
+var { name, age, ...etc } = user;
+console.log(name, age, etc.phone, etc.address);
--- workspace/ch01/ex01-24.js
+++ workspace/ch01/ex01-24.js
@@ -1,7 +1,20 @@
 // 전개 연산자
-var colors = ['two', 'three', 'four'];
-var user = {name: '김철수', age: 30};
+var colors = ["two", "three", "four"];
+var newColors = ["one", ...colors, "five"];
 
+console.log(newColors, colors === newColors);
+
+if (colors !== newColors) {
+  console.log("리렌더링");
+}
+
+var user = { name: "김철수", age: 30 };
+var newUser = { name: "김철수", age: 30 };
+
+console.log(newUser, user === newUser);
+if (user !== newUser) {
+  console.log("리렌더링2");
+}
 // 함수에서 사용
 
 // 배열 결합
